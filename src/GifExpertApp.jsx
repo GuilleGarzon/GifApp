daeccb6844e4e7a409900c8832286d046431eee9
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-11-03 10:41:08 -0500

    feat: add component GifGrid and add service

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+  const [categories, setCategories] = useState([ 'One Punch' ]);
 
   const onAddCategory = ( NewCategory ) => {  
     
@@ -15,15 +16,21 @@ export const GifExpertApp = () => {
   return (
     <>
       <h1>GifExpertApp</h1>
+
       <AddCategory
         // setCategories={ setCategories }
         onNewCategory={ onAddCategory } 
-      />      
-      <ol>
-        { categories.map(category => {
-          return <li key={ category }>{category}</li>
-        }) }
-      </ol>
+      />     
+      
+      { 
+        categories.map(category => (
+          <GifGrid 
+            key={ category }
+            category={ category }
+          />
+        ))
+      }
+      
     </>
   )
 }
